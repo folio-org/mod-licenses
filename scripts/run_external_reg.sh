7bf6ee10a006a3ad0dbe68e5994bb5b27e22999d
John Fereira <jaf30@cornell.edu>
2019-05-03 13:25:37 -0400

    derive jar file name automatically

--- scripts/run_external_reg.sh
+++ scripts/run_external_reg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo Start mod-licenses in external-register mode
+the_jar_file=`ls build/libs/mod-licenses*.jar | tail -n 1`
 
-java -jar build/libs/mod-licenses-1.1.jar -Xmx1G --server.port=8081 --grails.server.host=10.0.2.2 --dataSource.username=folio_admin --dataSource.password=folio_admin --dataSource.url=jdbc:postgresql://localhost:54321/okapi_modules
+echo Start mod-licenses in external-register mode using jarfile $the_jar_file
+
+java -jar $the_jar_file -Xmx1G --server.port=8081 --grails.server.host=10.0.2.2 --dataSource.username=folio_admin --dataSource.password=folio_admin --dataSource.url=jdbc:postgresql://localhost:54321/okapi_modules
 
