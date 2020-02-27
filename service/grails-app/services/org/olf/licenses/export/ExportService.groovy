3a3a6649ae1e1e255e61c436a9d1a5dd83397bd8
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2020-02-27 16:17:47 +0000

    WIP

---
+++ service/grails-app/services/org/olf/licenses/export/ExportService.groovy
@@ -0,0 +1,10 @@
+package org.olf.licenses.export
+import grails.gorm.transactions.Transactional
+
+/**
+ * Service for exporting resources.
+ */
+@Transactional(readOnly=true)
+public class ExportService {
+  
+}
