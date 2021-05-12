afeefe2fade44488a3bc5cb981a4e2511395374f
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-05-12 11:42:59 +0100

    removed service and tweaked controller

--- service/grails-app/controllers/org/olf/general/DashboardDefinitionsController.groovy
+++ service/grails-app/controllers/org/olf/general/DashboardDefinitionsController.groovy
@@ -1,14 +1,23 @@
 package org.olf.general
 
+import org.grails.io.support.PathMatchingResourcePatternResolver
+import org.grails.io.support.Resource
+
 import grails.gorm.multitenancy.CurrentTenant
 import groovy.util.logging.Slf4j
 
 @Slf4j
 @CurrentTenant
 class DashboardDefinitionsController  {
-  def dashboardDefinitionsService
+  PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver()
+  static String definitionsCache;
+
   public getDefinitions() {
-    respond dashboardDefinitionsService.getWidgetDefinitions();
+    if (!definitionsCache) {
+      Resource[] widgetDefs = resolver.getResources("classpath:sample_data/widgetDefinitions/*")
+      definitionsCache = "[${widgetDefs.collect { new String( it.getInputStream().readAllBytes()) }.join(',')}]"
+    }
+    render(text: definitionsCache, contentType: 'application/json')
   }
 }
 
