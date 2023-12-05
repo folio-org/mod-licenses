e5115aac29fa361d0de1655bc1a9fe3645eefca9
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-12-05 10:21:22 +0100

    Move btree_gin from initial to update-mod-license-5-1

--- service/grails-app/migrations/initial-customisations.groovy
+++ service/grails-app/migrations/initial-customisations.groovy
@@ -9,11 +9,4 @@ databaseChangeLog = {
       }
     }
   }
-  changeSet(author: "julianladisch", id: "2023-12-04T18:12:00") {
-    grailsChange {
-      change {
-        sql.execute('CREATE EXTENSION IF NOT EXISTS btree_gin WITH SCHEMA public;');
-      }
-    }
-  }
 }
